555e4fee4e59442be101135bf5a19ce191ceb399
EricLam404 <96885224+EricLam404@users.noreply.github.com>
2025-03-18 13:26:41 -0400

    Fix Auth0 integration

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,45 +1,51 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Background from "../../components/background/Background";
 import Navbar from "../../components/navbar/Navbar";
 import Hero from "../../components/hero/Hero";
 // import Recommendation from "../../components/recommendation/Recommendation";
 // import Footer from "../../components/footer/footer";
-import '../../App.css';
+import "../../App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const heroData = [
-  { text1: "Find Your", text2: "Fair and Square" },
-  { text1: "Where Everyone", text2: "Has a Fair Choice" },
-  { text1: "Honest Housing", text2: "For Good People" },
+    { text1: "Find Your", text2: "Fair and Square" },
+    { text1: "Where Everyone", text2: "Has a Fair Choice" },
+    { text1: "Honest Housing", text2: "For Good People" },
 ];
 
 const Main = () => {
-  const [heroCount, setHeroCount] = useState(0);
+    const [heroCount, setHeroCount] = useState(0);
+    const { user, isAuthenticated, isLoading, error} = useAuth0();
+    if (error) {
+      console.log("error: ", error);
+    }
+    if (isLoading) {
+        console.log("loading");
+    } else {
+        console.log("authenticated: ", isAuthenticated);
+        console.log(user);
+    }
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setHeroCount((count) => (count + 1) % heroData.length);
+        }, 15000);
+        return () => clearInterval(interval);
+    }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setHeroCount((count) => (count + 1) % heroData.length);
-    }, 15000);
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div className="body-container">
-      <div className="hero-container">
-        <Navbar/>
-        <Background/>
-        <Hero
-          heroData={heroData[heroCount]}
-          heroCount={heroCount}
-        />
-      </div>
-{/* 
+    return (
+        <div className="body-container">
+            <div className="hero-container">
+                <Navbar />
+                <Background />
+                <Hero heroData={heroData[heroCount]} heroCount={heroCount} />
+            </div>
+            {/* 
       <div className="lower-container">
         <Recommendation />
         <Footer />
       </div> */}
-    </div>
-  )
+        </div>
+    );
+};
 
-}
-
-export default Main
+export default Main;
