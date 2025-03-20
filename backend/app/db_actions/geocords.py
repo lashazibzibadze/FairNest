032c121e6be60a57218c07331cb8ddf22e79055b
EricLam404 <96885224+EricLam404@users.noreply.github.com>
2025-03-20 17:42:23 -0400

    Add geocoding functions to update listings with latitude and longitude

---
+++ backend/app/db_actions/geocords.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+import schemas, models
+
+def get_listings_to_update(db: Session):
+    return db.query(models.Address).filter(
+        models.Address.longitude == None,
+        models.Address.latitude == None
+    ).limit(10).all()
+
+def update_listing_with_geocords(db: Session, listing_id: int, lat: float, lng: float):
+    listing = db.query(models.Address).filter(models.Address.id == listing_id).first()
+    listing.latitude = lat
+    listing.longitude = lng
+    db.commit()
+    db.refresh(listing)
+    return listing
